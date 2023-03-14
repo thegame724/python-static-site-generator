359abab6cb4c5c8de27c474fdd11e8d776aaf38f
thegame724 <sdipasquale@outlook.com>
2023-03-14 19:29:19 -0400

    Fixed config dict in main function

--- ssg.py
+++ ssg.py
@@ -3,7 +3,7 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {"source": "source", "dest": "dest"}
+    config = {"source": source, "dest": dest}
     site = Site(**config).build()
 
 
