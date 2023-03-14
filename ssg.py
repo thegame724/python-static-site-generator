c4a0205e2d1b1045e3941006b1904b9ff80df497
thegame724 <sdipasquale@outlook.com>
2023-03-14 19:27:20 -0400

    Fixed issues with the Site Class module

--- ssg.py
+++ ssg.py
@@ -1,8 +1,10 @@
 import typer
 from ssg.site import Site
 
-def main(source = "content", dest = "dist"):
-    config = dict(source = "source", dest = "dest")
+
+def main(source="content", dest="dist"):
+    config = {"source": "source", "dest": "dest"}
     site = Site(**config).build()
+
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -6,11 +6,11 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest / Path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
     
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if path.is_dir:
+            if path.is_dir == True:
                 self.create_dir(path)
