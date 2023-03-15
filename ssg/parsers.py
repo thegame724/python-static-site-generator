c516c910b887b99eac47a41866f3239040c56c92
thegame724 <sdipasquale@outlook.com>
2023-03-15 14:15:18 -0400

    Fixing call to write

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,7 +40,7 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write(r"\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
 
 class ReStructuredTextParser(Parser):
     extensions = [ ".rst" ]
@@ -49,4 +49,4 @@ class ReStructuredTextParser(Parser):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write(r"\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
