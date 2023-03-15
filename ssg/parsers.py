3dfc0ff592fc36ca4949b998e8a6290a7f9d80d2
thegame724 <sdipasquale@outlook.com>
2023-03-14 20:25:00 -0400

    Fix errors in parsers and site classes

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,28 +3,28 @@ from pathlib import Path
 import shutil
 
 class Parser:
-    extensions = [] # type: List[str]
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
-        return self.extensions.__contains__(extension)
+        return extension in self.extensions
     
-    def parse(path, source, dest):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
-    def read(path):
-        with path.open('r') as file:
+    def read(self, path):
+        with open(path, "r") as file:
             return file.read()
         
     def write(self, path, dest, content, ext = ".html"):
-        full_path = self.dest / path.with_suffix(ext).name
-        with full_path.open('w') as file:
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
             file.write(content)
 
-    def copy(path, source, dest):
+    def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-    class ResourceParser(Parser):
-        extensions = [ ".jpg", ".png", ".gif", ".css", ".html" ]
+class ResourceParser(Parser):
+    extensions = [ ".jpg", ".png", ".gif", ".css", ".html" ]
 
-        def parse(path, source, dest):
-            super().copy(path, source, dest)
+    def parse(path, source, dest):
+        super().copy(path, source, dest)
--- ssg/site.py
+++ ssg/site.py
@@ -16,16 +16,16 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parser(self, path)
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
-            if parser.valid_entension():
+            if parser.valid_entension(extension):
                 return parser
             
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
-        if parser != None:
+        if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
